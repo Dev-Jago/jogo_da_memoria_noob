dda5cf7904b5a3a7cdcd36bde3f410194d828edc
Hiago Pinheiro <devj4g0@gmail.com>
2024-01-15 19:30:29 -0300

    primeiro commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,58 @@
+const emojis = [
+    "🤬",
+    "🤬",
+    "👹",
+    "👹",
+    "👺",
+    "👺",
+    "😈",
+    "😈",
+    "🤢",
+    "🤢",
+    "🤮",
+    "🤮",
+    "💀",
+    "💀",
+    "💩",
+    "💩"
+];
+let openCards = [];
+
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box)
+}
+
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if (openCards.length === 2) {
+        setTimeout(checkMatch, 500);
+    }
+    console.log(openCards);
+}
+
+function checkMatch() {
+    if (openCards[0].innerHTML.trim() === openCards[1].innerHTML.trim()) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+        alert("parabuanch, vousse eh buro 👍👍");
+    }
+}
